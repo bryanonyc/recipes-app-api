6914fc777f79f9bdc7bb3535c6a75beeceed3acc
bryanonyc <bryanonyc@yahoo.com>
2024-09-27 17:06:58 -0400

    Refactor handleError()

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { logEvents } from "./logger";
 import { PrismaClientKnownRequestError, PrismaClientUnknownRequestError, PrismaClientValidationError } from '@prisma/client/runtime/library';
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: Function) => {
+export const errorHandler = (err: Error, req: Request, res: Response) => {
     logEvents(`${err.name}: \t
         ${err.message}\t
         ${req.method}\t
@@ -10,28 +10,34 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Func
         ${req.headers.origin}`, 'errors.log'
     );
 
+    let errorMessage;
+    if (err instanceof PrismaClientKnownRequestError) {
+        errorMessage = err.meta;
+    } else {
+        errorMessage = err.message;
+    }
+
     const status = res.statusCode ? res.statusCode : 500;
     res
         .status(status)
-        .json({ message: err.message, isError: true });
+        .json({ message: errorMessage, isError: true });
 }
 
 export const handleError = (req: any, res: any, error: any) => {
-    logEvents(`${error.name}: \t
-        ${error.message}\t
-        ${req.method}\t
-        ${req.url}\t
-        ${req.headers.origin}`, 'errors.log'
-    );
-    let errorMessage;
-    if (error instanceof PrismaClientKnownRequestError) {
-        errorMessage = error.meta;
-    } else if (error instanceof PrismaClientUnknownRequestError) {
-        errorMessage = error.message;
-    } else if (error instanceof PrismaClientValidationError) {
-        errorMessage = error.message;
-    }
-    res
-        .status(500)
-        .send(errorMessage);
+    errorHandler(error, req, res);
+    // logEvents(`${error.name}: \t
+    //     ${error.message}\t
+    //     ${req.method}\t
+    //     ${req.url}\t
+    //     ${req.headers.origin}`, 'errors.log'
+    // );
+    // let errorMessage;
+    // if (error instanceof PrismaClientKnownRequestError) {
+    //     errorMessage = error.meta;
+    // } else {
+    //     errorMessage = error.message;
+    // }
+    // res
+    //     .status(500)
+    //     .json({ message: errorMessage });
 };
