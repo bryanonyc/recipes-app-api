dd1760ee3ff1c02db6a4c6b2a18b7b17f21760b1
bryanonyc <bryanonyc@yahoo.com>
2024-09-30 08:41:59 -0400

    When verifying the JWT, now check to see the user account is active before calling next

--- src/middleware/verifyJWT.ts
+++ src/middleware/verifyJWT.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import jwt, { TokenExpiredError } from 'jsonwebtoken';
 import { logEvents } from './logger';
+import { findUser } from '../controllers/auth';
 
 export const verifyJWT = (req: Request, res: Response, next: Function) => {
     const authHeader = (req.headers.authorization || req.headers.Authorization) as string;
@@ -18,7 +19,7 @@ export const verifyJWT = (req: Request, res: Response, next: Function) => {
         jwt.verify(
             token,
             process.env.ACCESS_TOKEN_SECRET!!,
-            (err: any, decoded: any) => {
+            async (err: any, decoded: any) => {
                 try {
                     if (err) {
                         return res
@@ -27,8 +28,25 @@ export const verifyJWT = (req: Request, res: Response, next: Function) => {
                                 message: 'Forbidden'
                             });
                     }
-
-                    next();
+                    // one last sanity check
+                    const user = await findUser(decoded.email);
+                    if (user === null) {
+                        res
+                            .status(401)
+                            .json({
+                                message: "No account found.",
+                                isError: true,
+                            });
+                    } else if (!user.isActive) {
+                        res
+                            .status(403)
+                            .json({
+                                message: "Your account is inactive.",
+                                isError: true,
+                            });
+                    } else {
+                        next();
+                    }
                 } catch (error: any) {
                     logEvents(`${error.name}: \t
                         ${error.message}\t
