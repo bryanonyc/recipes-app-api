71df83f14bda3e167753d9e3eee5f81c1417001a
bryanonyc <bryanonyc@yahoo.com>
2024-10-02 16:22:20 -0400

    Rename email column to username

---
+++ prisma/migrations/20241002200759_replace_email_column_with_username/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `email` on the `users` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[username]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `username` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "users_email_key";
+
+-- AlterTable
+ALTER TABLE "users" RENAME COLUMN "email" TO "username";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
